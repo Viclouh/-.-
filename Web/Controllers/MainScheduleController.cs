9c1fcb9f0f4679c89eaa16073142504223058bae
Lif0n <t4sty.c4ke@gmail.com>
2023-12-11 04:43:03 +0400

    asdasd

--- Web/Controllers/MainScheduleController.cs
+++ Web/Controllers/MainScheduleController.cs
@@ -2,6 +2,7 @@
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
@@ -10,10 +11,23 @@ namespace Web.Controllers
 		public async Task<IActionResult> Index()
 		{
 			Context context = new Context();
+
+
 			return View(context.Specialities
 				.Include(x=>x.Courses)
 				.ThenInclude(x=>x.Groups).ToList()
 				);
+		}
+		public async Task<IActionResult> Schedule()
+		{
+			Context context = new Context();
+			MainScheduleViewModel model = new MainScheduleViewModel
+			{
+				Specialities = context.Specialities.ToList(),
+				Groups = context.Groups.ToList(),
+				Teachers = context.Teachers.ToList(),
+			};
+			return PartialView();
 		}
 	}
 }
