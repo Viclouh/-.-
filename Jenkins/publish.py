90015549d3d6f580f96d7cfcea80aaf99add9669
Илья Силов <iliya.silov@gmail.com>
2024-04-09 16:11:31 +0300

    фыв

--- Jenkins/publish.py
+++ Jenkins/publish.py
@@ -1,5 +1,6 @@
-mport os
+import os
 import requests
+import sys
 
 def send_apk_to_telegram(bot_token, chat_id, apk_path):
     url = f'https://api.telegram.org/bot{bot_token}/sendDocument'
@@ -11,14 +12,22 @@ def send_apk_to_telegram(bot_token, chat_id, apk_path):
     else:
         print("Failed to send APK to Telegram. Status code:", response.status_code)
 
-# Укажите ваш токен бота и chat_id получателя
-bot_token = '6793739177:AAFRJZH4JvBaxzHV-nYsGT33lxw_7mDcZaI'
-chat_id = '-1002065699771'
+if __name__ == "__main__":
+    # Укажите ваш токен бота и chat_id получателя
+    bot_token = '6793739177:AAFRJZH4JvBaxzHV-nYsGT33lxw_7mDcZaI'
+    chat_id = '-1002065699771'
 
-# Получение пути к текущей директории
-current_dir = os.path.dirname(os.path.realpath(__file__))
+    # Проверяем, был ли передан путь к APK в качестве аргумента командной строки
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <apk_path>")
+        sys.exit(1)
+    
+    # Получаем путь к APK из аргумента командной строки
+    apk_path = sys.argv[1]
 
-# Путь к APK файлу
-apk_path = os.path.join(current_dir, 'akvt_raspisanie/build/app/outputs/flutter-apk/app-release.apk')
+    # Проверяем, существует ли указанный файл APK
+    if not os.path.exists(apk_path):
+        print("APK file not found:", apk_path)
+        sys.exit(1)
 
-send_apk_to_telegram(bot_token, chat_id, apk_path)
+    send_apk_to_telegram(bot_token, chat_id, apk_path)
