bf8a02c7151bfb9e89a4502f91288ba405d94ffa
Lif0n <115246230+Lif0n@users.noreply.github.com>
2024-05-13 19:08:07 +0300

    Update TeacherService.cs

--- API/Services/TeacherService.cs
+++ API/Services/TeacherService.cs
@@ -24,12 +24,12 @@ namespace API.Services
         {
             if (query.IsNullOrEmpty())
             {
-                return _context.Teacher.ToList();
+                return _context.Teacher.OrderBy(t => t.Surname).ToList();
             }
 
             return _context.Teacher.Where(t => t.Surname.ToLower().Contains(query.ToLower())
             || t.Name.ToLower().Contains(query.ToLower())
-            || t.Patronymic.ToLower().Contains(query.ToLower())).ToList();
+            || t.Patronymic.ToLower().Contains(query.ToLower())).OrderBy(t => t.Surname).ToList();
         }
 
         public Teacher Put(Teacher newTeacher)
