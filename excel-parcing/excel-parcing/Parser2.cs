e624c11060bfabeb5ff5f691343ec230724ef750
Lif0n <t4sty.c4ke@gmail.com>
2024-06-13 01:50:01 +0400

    teacher-subject sdelat' nado ilya prover

---
+++ excel-parcing/excel-parcing/Parser2.cs
@@ -0,0 +1,41 @@
+﻿using excel_parcing.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excel_parcing
+{
+    internal class Parser2
+    {
+        private ParseConfig _config;
+
+        public Parser2()
+        {
+            _config = new ParseConfig
+            {
+                StartColumn = 3,
+                StartRow = 10,
+                EndColumn = 36,
+                SkipRows = new[] { 34, 59, 84, 109, 134 },
+                CountRows = 159
+            };
+        }
+
+        public string Path { get; set; }
+        public List<Cabinet> Cabinets = new List<Cabinet>();
+        public List<Course> Courses = new List<Course>();
+        public List<Models.Group> Groups = new List<Models.Group>();
+        public List<Subject> Subjects = new List<Subject>();
+        public List<Teacher> Teachers = new List<Teacher>();
+        public List<TeacherSubject> Teacher_Subjects = new List<TeacherSubject>();
+        public List<Main_Lesson> Main_Lessons = new List<Main_Lesson>();
+
+        public void ParseVse()
+        {
+            
+        }
+    }
+}
