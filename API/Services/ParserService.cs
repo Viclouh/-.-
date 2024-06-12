b54db4457832edc8343846dd20ac8a6d8b4fd4cb
Lif0n <t4sty.c4ke@gmail.com>
2024-06-13 02:55:15 +0400

    sha

---
+++ API/Services/ParserService.cs
@@ -0,0 +1,17 @@
+﻿
+namespace API.Services
+{
+    public class ParserService
+    {
+        private Database.Context _context;
+
+        public ParserService(Database.Context context)
+        {
+            _context = context;
+        }
+        public void Parse(int year, int semester, int status, int department)
+        {
+
+        }
+    }
+}
