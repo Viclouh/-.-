1007ce2e7dedd109e41f5fbe707a921e3b1e71e0
Lif0n <t4sty.c4ke@gmail.com>
2024-06-18 08:14:28 +0400

    lgt service and controller

---
+++ API/Controllers/LessonGroupTeacherController.cs
@@ -0,0 +1,20 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class LessonGroupTeacherController : ControllerBase
+    {
+        private readonly LessonGroupTeacherService _lessonGroupTeacherService;
+        public LessonGroupTeacherController(LessonGroupTeacherService lessonGroupTeacherService)
+        {
+            _lessonGroupTeacherService = lessonGroupTeacherService;
+        }
+        [HttpGet]
+        public IActionResult Get(int? teacherId, int? groupId, int? subjectId)
+        {
+            return StatusCode(200, _lessonGroupTeacherService.Get(groupId, subjectId, teacherId));
+        }
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -63,6 +63,7 @@ namespace API
             builder.Services.AddScoped<TeacherSubjectService>();
             builder.Services.AddScoped<LessonGroupService>();
             builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<LessonGroupTeacherService>();
 
             builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
             {
